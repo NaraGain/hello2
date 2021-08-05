c5888dc5cb882ea5572fe6043d3adf86b5299aef
Bun Nara <bunnarachoc@gmail.com>
2021-08-05 20:20:41 +0700

    add new code on myFile.js

--- myFile.js
+++ myFile.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+function getFile(){
+    let xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            console.log(xhr.responseText);
+
+        }else{
+            console.error(this.status+ " Error Conncetion ");
+        }
+    }
+    xhr.open("GET","myFile.txt",true)
+    xhr.send()
+}
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector('button').onclick = getFile
+})
