1c247f1328bac5881151271d499c1a669de94fb2
Bun Nara <bunnarachoc@gmail.com>
2021-10-17 21:00:22 +0700

    add new World to myFile.java

--- myFile.java
+++ myFile.java
@@ -1,8 +1,9 @@
 public class myFile{
   
   
-  public static void main(string[] args){
+  public static void main(String[] args){
     
    System.out.println("Hello World"); 
+   System.out.println("MY name is Dara");
   }
 }
